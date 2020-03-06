4f639f26cea42b2d141b5a86ba07bf4909047efc
Mathieu Dutour <mathieu@dutour.me>
2020-03-06 09:33:41 +0100

    emit an index.js when convering workspace

---
+++ examples/generated/js/index.js
@@ -0,0 +1,29 @@
+var __lona_import_0 = require("./Colors");
+Object.keys(__lona_import_0).forEach(function (key) {
+  Object.defineProperty(module.exports, key, {
+    enumerable: true,
+    get: function get() {
+      return __lona_import_0[key];
+    }
+  });
+})
+
+var __lona_import_1 = require("./Shadows");
+Object.keys(__lona_import_1).forEach(function (key) {
+  Object.defineProperty(module.exports, key, {
+    enumerable: true,
+    get: function get() {
+      return __lona_import_1[key];
+    }
+  });
+})
+
+var __lona_import_2 = require("./TextStyles");
+Object.keys(__lona_import_2).forEach(function (key) {
+  Object.defineProperty(module.exports, key, {
+    enumerable: true,
+    get: function get() {
+      return __lona_import_2[key];
+    }
+  });
+})
